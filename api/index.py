47898b9a17b7e2b6c3cc031dac7049f9b64a9267
vitks <49155106+vitcas@users.noreply.github.com>
2025-09-27 15:30:21 -0300

    me diga entaaaao

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template
 from flask_cors import CORS
 import json
 import math
